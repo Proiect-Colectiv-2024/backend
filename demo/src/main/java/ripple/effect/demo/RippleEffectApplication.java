1fe04e5d70442dfbad764673b83673f5c8447e6b
Calin <acalin729@gmail.com>
2024-10-11 12:24:41 +0300

    initial commit

---
+++ demo/src/main/java/ripple/effect/demo/RippleEffectApplication.java
@@ -0,0 +1,13 @@
+package ripple.effect.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RippleEffectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RippleEffectApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/ripple/effect/demo/RippleEffectApplicationTests.java
@@ -0,0 +1,13 @@
+package ripple.effect.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RippleEffectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
